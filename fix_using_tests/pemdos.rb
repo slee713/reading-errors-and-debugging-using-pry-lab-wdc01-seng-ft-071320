8c0030d87831898e92b7306649ac98e822b14fc5
Steven Lee <steven.lee1393@gmail.com>
2020-06-29 19:11:07 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,16 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
+  result = string
   if string[0] == "s"
-    10 * "s" + string
+
+    10.times do
+      result.insert(0,'s')
+    end 
+    result
   else
-    string
+    result
   end
 end
+
+snake_it_up("surprise!")
